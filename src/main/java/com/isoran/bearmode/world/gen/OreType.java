7b5a01b7914cbbe84c338abc42b1b7502a5c6454
ionutsoran <ionutsoran@gmail.com>
2021-06-24 12:14:33 +0300

    first ore generation implementation

---
+++ src/main/java/com/isoran/bearmode/world/gen/OreType.java
@@ -0,0 +1,50 @@
+package com.isoran.bearmode.world.gen;
+
+import com.isoran.bearmode.block.ModBlocks;
+import net.minecraft.block.Block;
+
+public enum OreType {
+
+    COPPER(ModBlocks.COPPER_ORE.get(), 8, 25, 50);
+
+    private final Block block;
+
+    private final int maxVeinSize;
+    private final int minHeight;
+    private final int maxHeight;
+    OreType(Block block, int maxVeinSize, int minHeight, int maxHeight)
+    {
+        this.block = block;
+        this.maxVeinSize = maxVeinSize;
+        this.minHeight = minHeight;
+        this.maxHeight = maxHeight;
+    }
+
+    public Block getBlock() {
+        return block;
+    }
+
+    public int getMaxVeinSize() {
+        return maxVeinSize;
+    }
+
+    public int getMinHeight() {
+        return minHeight;
+    }
+
+    public int getMaxHeight() {
+        return maxHeight;
+    }
+
+    public static OreType get(Block block)
+    {
+        for(OreType ore: values())
+        {
+            if(block == ore.block)
+            {
+                return ore;
+            }
+        }
+        return null;
+    }
+}
